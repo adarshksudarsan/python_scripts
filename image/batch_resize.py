fc5e70759b550b02421c5305fcdcce808fda276f
mis_takes <adarshksudarshan@gmail.com>
2019-11-28 12:45:26 +0530

    Add files via upload

---
+++ image/batch_resize.py
@@ -0,0 +1,20 @@
+#image resize script
+from PIL import Image
+import os
+import sys
+
+directory = '/home/vvdn/Desktop/image_manipulations/images'
+a,b=input('Enter required width * height seperated by "*"').split("*")
+
+for file_name in os.listdir(directory):
+    image = Image.open(os.path.join(directory, file_name))
+
+    x,y = image.size
+    new_dimensions = (int(a),int(b)) #dimension set here
+    output = image.resize(new_dimensions, Image.ANTIALIAS)
+
+    output_file_name = os.path.join(directory, "small_" + file_name)
+    output.save(output_file_name, "JPEG", quality = 95)
+
+print("All done")
+
