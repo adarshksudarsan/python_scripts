60b79da0dd298bd686a0861c6ba57f55f9ebab36
mis_takes <adarshksudarshan@gmail.com>
2019-12-04 17:27:24 +0530

    new entries

---
+++ addons/find_a_file.py
@@ -0,0 +1,23 @@
+"""
+#######################################################
+script to search files in your directory(ver 1)
+#######################################################
+"""
+
+import os
+import fnmatch
+import sys
+
+def find(search, path):
+  result=[]
+  for root, dirs, files in os.walk(path):
+    for name in files:
+      if fnmatch.fnmatch(name,search):
+        result.append(os.path.join(root, name)) 
+  return result
+"""
+here i have used "*" in print statement ,it is a way of displaying list in python
+and to view in seperate lines i have used "sep="\n""
+""" 
+print(*find('*'+sys.argv[1],sys.argv[2]),sep="\n")
+
